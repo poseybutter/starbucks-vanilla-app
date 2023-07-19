585e097261805f9c44de11f719a20f76652c178e
Lee jieun <110350512+poseybutter@users.noreply.github.com>
2023-07-19 09:46:38 +0900

    5th commit
    Swiper, GSAP(ScrollToPlugin), ScrollMagic
    Footer 영역까지 완성

--- js/main.js
+++ js/main.js
@@ -25,8 +25,9 @@ searchInputEl.addEventListener('blur', function () {
 /**
  * 페이지 스크롤에 따른 요소 제어
  */
-// BADGES
+// BADGES, To Top 버튼
 const badgeEl = document.querySelector('header .badges');
+const toToplEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function () {
   // console.log(window.scrollY)
@@ -38,6 +39,10 @@ window.addEventListener('scroll', _.throttle(function () {
       opacity: 0,
       display: 'none',
     });
+    // To Top 버튼 보이기 !
+    gsap.to(toToplEl, .2, {
+      x: 0,
+    });
   } else {
     // 배지 보이기
     // badgeEl.style.display = 'block'
@@ -45,9 +50,19 @@ window.addEventListener('scroll', _.throttle(function () {
       opacity: 1,
       display: 'block',
     });
+    // To Top 버튼 숨기기 !
+    gsap.to(toToplEl, .2, {
+      x: 100,
+    });
   }
 }, 300));
 // _.throttle(함수, 시간)
+
+toToplEl.addEventListener('click', function () {
+  gsap.to(window, .7, {
+    scrollTo: 0,
+  });
+});
 
 
 /**
@@ -93,6 +108,19 @@ new Swiper('.promotion .swiper', {
   },
 });
 
+new Swiper('.awards .swiper', {
+  // direction: 'horizontal', // 수평 슬라이드
+  autoplay: true, // 자동 재생 여부
+  loop: true, // 반복 재생 여부
+  spaceBetween: 30, // 슬라이드 사이 여백
+  slidesPerView: 5, // 한 번에 보여줄 슬라이드 개수
+  // slidesPerGroup: 5, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
+  navigation: { // 슬라이드 이전/다음 버튼 사용 여부
+    prevEl: '.awards .swiper-button-prev', // 이전 버튼 선택자
+    nextEl: '.awards .swiper-button-next' // 다음 버튼 선택자
+  }
+})
+
 
 /**
  * // 슬라이드 토글
@@ -135,3 +163,28 @@ function floatingObject(selector, delay, size) {
 floatingObject('.floating1', 1, 15);
 floatingObject('.floating2', .5, 15);
 floatingObject('.floating3', 1.5, 20);
+
+
+/**
+ * 요소가 화면에 보여짐 여부에 따른 요소 관리
+ */
+// 관리할 요소들 검색!
+const spyEls = document.querySelectorAll('section.scroll-spy')
+// 요소들 반복 처리!
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({ // 감시할 장면(Scene)을 추가
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8 // 화면의 80% 지점에서 보여짐 여부 감시
+    })
+    .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+    .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당(필수!)
+})
+
+
+/**
+ * 올해가 몇 년도 인지 계산
+ */
+const thisYear = document.querySelector('.this-year')
+thisYear.textContent = new Date().getFullYear()
+// 현재 년도의 정보 데이터가 반환됨
